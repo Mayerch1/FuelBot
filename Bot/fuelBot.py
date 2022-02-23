2685052bf57c5c10ed1e7443353c1c813310e97c
Christian <it@cj-mayer.de>
2022-02-24 00:23:34 +0100

    use new help/servercout ext modules

--- Bot/fuelBot.py
+++ Bot/fuelBot.py
@@ -5,6 +5,11 @@ import traceback
 import sys
 
 from pathlib import Path
+from discord.ext.help import Help, HelpElement, HelpPage
+from discord.ext.servercount import ServerCount
+
+FEEDBACK_CHANNEL = 890973072662884383
+FEEDBACK_MENTION = 872107119988588566
 
 
 logging.basicConfig(level=logging.INFO) # general 3rd party
@@ -16,6 +21,9 @@ dc_logger = logging.getLogger('discord')
 dc_logger.setLevel(logging.WARNING) # discord lib
 log = logging.getLogger('FuelBot')
 log.setLevel(logging.DEBUG) # own code
+
+log = logging.getLogger('ext.servercount')
+log.setLevel(logging.DEBUG) # debug
 
 dc_logger.addHandler(handler)
 log.addHandler(handler)
@@ -56,6 +64,7 @@ async def on_ready():
     log.info(bot.user)
     log.info(bot.user.id)
     log.info('----------------')
+    config_help()
 
 
 @bot.event
@@ -78,12 +87,38 @@ async def on_error(event_method, *args, **kwargs):
 # # Commands
 # ############
 
+def set_tokens():
+    ServerCount.init(bot, 'fuelBot (https://github.com/Mayerch1/FuelBot)')
+    ServerCount.set_token_dir('tokens')
+
+
+def config_help():
+
+    custom_footer = 'If you like this bot, you can leave a vote at [top.gg](https://top.gg/bot/890731736252686337).\n'\
+                                'If you find a bug contact us on [Github](https://github.com/Mayerch1/QuoteBot) or join the support server.'
+
+    Help.init_help(bot, auto_detect_commands=True)
+
+    Help.set_default_footer(custom_footer)
+    Help.set_feedback(FEEDBACK_CHANNEL, FEEDBACK_MENTION)
+    Help.invite_permissions(
+        discord.Permissions(attach_files=True)
+    )
+    Help.support_invite('https://discord.gg/Xpyb9DX3D6')
+    Help.set_tos_file('legal/tos.md')
+    Help.set_privacy_file('legal/privacy.md')
+    Help.set_github_url('https://github.com/Mayerch1/FuelBot')
+
 
 def main():
     for filename in os.listdir(Path(__file__).parent / 'cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
 
+    bot.load_extension('discord.ext.help.help')
+    bot.load_extension('discord.ext.servercount.servercount')
+
+    set_tokens()
     bot.run(token)
 
 
